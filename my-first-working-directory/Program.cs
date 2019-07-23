cc6f1e9cee256dde9fc4feba36083712274b2070
Kristof Mattei <csharpfan@users.noreply.github.com>
2019-07-22 21:36:34 -0700

    Print working directory and startup directory

---
+++ my-first-working-directory/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace MyFirstWorkingDirectory
+{
+    using System;
+    using System.IO;
+
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            string baseDirectory = AppContext.BaseDirectory;
+
+            string currentDirectory = $"{Directory.GetCurrentDirectory()}\\";
+
+            Console.WriteLine($"Current AppContext.BaseDirectory is {baseDirectory}");
+            Console.WriteLine($"Current working directory is {currentDirectory}");
+            Console.WriteLine($"Both are {(baseDirectory == currentDirectory ? "" : "not ")}the same");
+
+            Console.ReadLine();
+        }
+    }
+}
